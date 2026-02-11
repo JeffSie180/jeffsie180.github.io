bf61b60bce006ae3dfe2442d1b83edc34fa2c967
謝銘鴻 <u01@JeffSie-Mac-2.local>
2026-02-11 12:39:55 +0800

    Site updated: 2026-02-11 12:39:54

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -1,62 +1,74 @@
-function electric_clock_injector_config(){var c=document.getElementsByClassName("sticky_layout")[0];console.log("已挂载electric_clock"),c.insertAdjacentHTML("afterbegin",'<div class="card-widget card-clock"><div class="card-glass"><div class="card-background"><div class="card-content"><div id="hexo_electric_clock"><img id="card-clock-loading" src="/clock/images/weather/loading.gif" style="height: 120px; width: 100%;" data-ll-status="loading" class="entered loading"></div></div></div></div></div>')}document.getElementsByClassName("sticky_layout")[0]&&(location.pathname,1)&&electric_clock_injector_config()
+(function () {
+  var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-console.log(returnCitySN["cip"])
-fetch('https://wttr.in/'+returnCitySN["cip"]+'?format="%l+\\+%c+\\+%t+\\+%h"').then(res=>res.text()).then(
-    data => {
-        var res_text = data.replace(/"/g,'').replace(/\+/g,'').replace(/,/g,'\\').replace(/ /g,'').replace(/°C/g,'');
-        res_list = res_text.split('\\');
-        var clock_box = document.getElementById('hexo_electric_clock');
-        clock_box_html = `
-  <div class="clock-row">
-<span id="card-clock-clockdate" class="card-clock-clockdate"></span>
-<span class="card-clock-weather">${res_list[2]} ${res_list[3]} *C</span>
-<span class="card-clock-humidity">💧 ${res_list[4]}</span>
-</div>
-  <div class="clock-row"><span id="card-clock-time" class="card-clock-time"></span></div>
+  function zeroPadding(num, digit) {
+    var zero = '';
+    for (var i = 0; i < digit; i++) zero += '0';
+    return (zero + num).slice(-digit);
+  }
 
-  <div class="clock-row">
- <!-- <span class="card-clock-ip">${returnCitySN["cip"]}</span>
-<span class="card-clock-location">${res_list[0]}</span>
-  <span id="card-clock-dackorlight" class="card-clock-dackorlight"></span> -->
-</div>
-`;
-        var week = ['SUN', 'MON', 'TUE', 'WED','THU' ,'FRI', 'SAT'];
-        var card_clock_loading_dom = document.getElementById('card-clock-loading');
-        card_clock_loading_dom.innerHTML='';
-        clock_box.innerHTML= clock_box_html;
-        function updateTime() {
-            var cd = new Date();
-            var card_clock_time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
-            var card_clock_date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' '+week[cd.getDay()];
-            var card_clock_dackorlight = cd.getHours();
-            var card_clock_dackorlight_str;
-            if(card_clock_dackorlight >12) {
-                card_clock_dackorlight -= 12;
-                card_clock_dackorlight_str = " PM";
-            }else{
-                card_clock_dackorlight_str = " AM";
-            }
-            if(document.getElementById('card-clock-time')){
-            var card_clock_time_dom = document.getElementById('card-clock-time');
-            var card_clock_date_dom = document.getElementById('card-clock-clockdate');
-            var card_clock_dackorlight_dom = document.getElementById('card-clock-dackorlight');
-            card_clock_time_dom.innerHTML= card_clock_time;
-            card_clock_date_dom.innerHTML= card_clock_date;
-            if(card_clock_dackorlight_dom) card_clock_dackorlight_dom.innerHTML= card_clock_dackorlight_str
-                }
-        }
+  function updateTime() {
+    var cd = new Date();
+    var timeStr = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    var dateStr = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth() + 1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    var ampm = cd.getHours() >= 12 ? ' PM' : ' AM';
 
-        function zeroPadding(num, digit) {
-            var zero = '';
-            for(var i = 0; i < digit; i++) {
-                zero += '0';
-            }
-            return (zero + num).slice(-digit);
-        }
+    var timeDom = document.getElementById('card-clock-time');
+    var dateDom = document.getElementById('card-clock-clockdate');
+    var ampmDom = document.getElementById('card-clock-dackorlight');
+    if (timeDom) timeDom.innerHTML = timeStr;
+    if (dateDom) dateDom.innerHTML = dateStr;
+    if (ampmDom) ampmDom.innerHTML = ampm;
+  }
 
-           var timerID = setInterval(updateTime, 1000);
-           updateTime();
+  function initClock(weatherHtml) {
+    var clockBox = document.getElementById('hexo_electric_clock');
+    if (!clockBox) return;
 
-        console.log(res_list)
+    clockBox.innerHTML =
+      '<div class="clock-row">' +
+        '<span id="card-clock-clockdate" class="card-clock-clockdate"></span>' +
+        weatherHtml +
+      '</div>' +
+      '<div class="clock-row"><span id="card-clock-time" class="card-clock-time"></span></div>' +
+      '<div class="clock-row">' +
+        '<span id="card-clock-dackorlight" class="card-clock-dackorlight"></span>' +
+      '</div>';
+
+    updateTime();
+    setInterval(updateTime, 1000);
+  }
+
+  function loadWeather() {
+    var ip = '';
+    try {
+      if (typeof returnCitySN !== 'undefined' && returnCitySN.cip) {
+        ip = returnCitySN.cip;
+      }
+    } catch (e) {}
+
+    if (!ip) {
+      initClock('');
+      return;
     }
-)
+
+    fetch('https://wttr.in/' + ip + '?format="%l+\\+%c+\\+%t+\\+%h"')
+      .then(function (res) { return res.text(); })
+      .then(function (data) {
+        var res_text = data.replace(/"/g, '').replace(/\+/g, '').replace(/,/g, '\\').replace(/ /g, '').replace(/°C/g, '');
+        var res_list = res_text.split('\\');
+        var weatherHtml = '<span class="card-clock-weather">' + (res_list[2] || '') + ' ' + (res_list[3] || '') + ' °C</span>' +
+          '<span class="card-clock-humidity">💧 ' + (res_list[4] || '') + '</span>';
+        initClock(weatherHtml);
+      })
+      .catch(function () {
+        initClock('');
+      });
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', loadWeather);
+  } else {
+    loadWeather();
+  }
+})();
